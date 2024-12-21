beeb51ee3fe17f17a3c5535ca46f248aa9023acf
Prestdigitador <devporcell@protonmail.com>
2024-12-20 22:15:29 -0300

    Update var.cpp

--- var.cpp
+++ var.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
 string SalNombre = "Hola, mi nombre es Alonso. ";
-Edad = 15; // Mitad actual
-Duplo = 15; // Duplica, para fines didácticos.
-  int edad = Edad + Duplo
+int Edad = 15; // Mitad actual
+int Duplo = 15; // Duplica, para fines didácticos.
+  int edad = Edad + Duplo;
 cout << SalNombre << "tengo " << edad << " años.";
 return 0;
 }
