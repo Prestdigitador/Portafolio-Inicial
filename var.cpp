842a3642f1e68bfbaf93fe2f3f59c926cad6c8f1
Prestdigitador <devporcell@protonmail.com>
2024-12-20 22:10:58 -0300

    Create var.cpp

---
+++ var.cpp
@@ -0,0 +1,12 @@
+//Var.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+int numero;
+string SalNombre = "Hola, mi nombre es Alonso. ";
+numero = 8; // imprime 8
+numero = 30; // ahora imprime 30
+cout << SalNombre << "tengo " << numero << " años.";
+return 0;
+}
